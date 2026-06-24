7e16028948f8e75b12f03dfac08a6c21ab048127
José Antônio <jasaj.ti@gmail.com>
2026-06-23 21:32:53 -0300

    comit 2

---
+++ src/main/java/com/github/app/controller/AlunoController.java
@@ -0,0 +1,60 @@
+package com.github.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import com.github.app.model.aluno.AlunoRepository;
+import com.github.app.model.aluno.DadosAtualizacaoAluno;
+import com.github.app.model.aluno.DadosCadastroAluno;
+import com.github.app.model.aluno.DadosListagemAluno;
+import com.github.app.model.aluno.Aluno;
+
+import jakarta.transaction.Transactional;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody DadosCadastroAluno dados) {
+        repository.save(new Aluno(dados));
+    }
+
+    @GetMapping("/todos")
+    public List<Aluno> listarTodos() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/listar")
+    public List<DadosListagemAluno> listar() {
+        return repository.findAll().stream().map(DadosListagemAluno::new).toList();
+    }
+
+    @GetMapping
+    public Page<DadosListagemAluno> listarPorPagina(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemAluno::new);
+    }
+
+    // ERRO 5: deveria ser @PutMapping — @PostMapping fará este endpoint conflitar com o cadastrar()
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody DadosAtualizacaoAluno dados) {
+        var aluno = repository.getReferenceById(dados.id());
+        aluno.atualizarInformacoes(dados);
+    }
+    //ERRO 7: O ID de "Aluno" é "Integer", mas o controller recebia "String". Isso também estava ligado ao erro do "AlunoRepository".
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Integer id) {
+        repository.deleteById(id);
+    }
+}
